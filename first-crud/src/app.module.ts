50f3bee902aef797205c92adba149cd3a08b6cc3
PavanButke <butkepavan55@gmail.com>
2024-01-18 23:20:21 +0530

    TypeOrm DB connections

--- first-crud/src/app.module.ts
+++ first-crud/src/app.module.ts
@@ -4,9 +4,10 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [BookModule , UserModule , AuthModule,
+  imports: [BookModule , UserModule , AuthModule, DatabaseModule,
     ConfigModule.forRoot({
       isGlobal:true,
       envFilePath: ".local.env"
