7a7a0d35258837dbe9b50de1edd136b2dfdfb139
PavanButke <butkepavan55@gmail.com>
2024-01-18 23:53:26 +0530

    config with db connection

--- first-crud/src/app.module.ts
+++ first-crud/src/app.module.ts
@@ -3,19 +3,33 @@ import { BookModule } from './book/book.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [BookModule , UserModule , AuthModule, DatabaseModule,
-    ConfigModule.forRoot({
-      isGlobal:true,
-      envFilePath: ".local.env"
-      //envFilePath: ".prod.env"
-    })
+    TypeOrmModule.forRootAsync(
+      {
+        imports : [ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: ".local.env"
+        })],
+
+    
+      useFactory: (configServ : ConfigService)=>({
+        type: 'postgres',
+        host: configServ.get('DB_HOST'),
+        port: configServ.get<number>('DB_PORT'),
+        username: configServ.get('DB_USERNAME'),
+        password: configServ.get('DB_PASSWORD'),
+        database: configServ.get('DB_DATABASE'),
+        synchronize: configServ.get('DB_SYNC') 
+      }),
+      inject: [ConfigService]
+  })
   ],
   controllers: [AppController],
   providers: [],
-  
 })
 export class AppModule {}
